8cd311dfb623dbb352c6ec68104af2300f9912cc
Angeli Mae V. Udto <angelimae.udto@g.msuiit.edu.ph>
2025-09-18 16:03:51 +0800

    Updated README.md with new documentation and screenshots

--- src/child_echo.c
+++ src/child_echo.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <windows.h>
 
@@ -10,17 +9,5 @@ int main(int argc, char *argv[]) {
     // Delay for 60 seconds so you can observe/control the process
     Sleep(60000); // 60000 milliseconds = 60 seconds
     return 0;
-=======
-#include <stdio.h>
-#include <windows.h>
 
-int main(int argc, char *argv[]) {
-    DWORD pid = GetCurrentProcessId();
-    DWORD ppid = argc > 1 ? atoi(argv[1]) : 0;
-    printf("Child PID: %lu\n", pid);
-    printf("Parent PID (passed): %lu\n", ppid);
-    // Delay for 60 seconds so you can observe/control the process
-    Sleep(60000); // 60000 milliseconds = 60 seconds
-    return 0;
->>>>>>> fce7546a471eb97f11b2e3d1c1fa9d4ac784e9bf
 }
